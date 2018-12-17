2800c565087351517106327bf7e302a79f384e5c
Julia-Markelova <usikmarkelova@mail.ru>
2018-12-18 00:35:36 +0300

    pullenti organisations is ok, need to fix strcmp

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import stats
 from doc_info import DocumentInfo
 from extract_text import PlainText
 from ner_natasha import extract_organizations, extract_money
-from ner_pullenti import extract_money
+from ner_pullenti import extract_money_org
 from xml_parser import ExtractXML, TagNames
 
 directory = '/home/yulia/Рабочий стол/digdes/Uploads'
@@ -32,15 +32,17 @@ for files in os.listdir(directory):
         text = p.extract_doc_text(file).decode('utf-8')
 
         # PULLENTI
+        doc = DocumentInfo(file)
         Processor([])
         processor = ProcessorService.create_processor()
-        extract_money(text, processor)
+        extract_money_org(text, processor, doc)
 
         # NATASHA
         # doc = DocumentInfo(file)
         # doc = extract_organizations(text, file, doc)
         # doc = extract_money(text, doc)
-        # docs.append(doc)
+
+        docs.append(doc)
 
         print(file_counter, file)
         file_counter += 1
@@ -50,20 +52,20 @@ for files in os.listdir(directory):
 for document in docs:
     print(document.doc_name)
     xml = ExtractXML(document.doc_name)
-    # xml.get_value(TagNames.ORGANISATION)
-    # xml.get_value(TagNames.PARTNER)
+    xml.get_value(TagNames.ORGANISATION)
+    xml.get_value(TagNames.PARTNER)
     xml.get_value(TagNames.MONEY)
-    # union = stats.strict_include(xml.org_tags, document.companies)
-    # is_not_empty = bool(union)
-    # if is_not_empty:
-    #     print("union:", union)
-    #
-    # union = stats.include(xml.org_tags, document.companies)
-    # is_not_empty = bool(union)
-    # if is_not_empty:
-    #     print("union: inc!", union)
-    # else:
-    #     empty_union_org_counter += 1
+    union = stats.strict_include(xml.org_tags, document.companies)
+    is_not_empty = bool(union)
+    if is_not_empty:
+        print("union:", union)
+
+    union = stats.include(xml.org_tags, document.companies)
+    is_not_empty = bool(union)
+    if is_not_empty:
+        print("union: inc!", union)
+    else:
+        empty_union_org_counter += 1
 
     union = stats.include_money(xml.money_tags, document.money)
     is_not_empty = bool(union)
@@ -72,7 +74,7 @@ for document in docs:
     else:
         empty_union_money_counter += 1
 
-    # print(xml.org_tags)
+    print(xml.org_tags)
     print(xml.money_tags)
 
     for k in document.companies:
--- ner_pullenti.py
+++ ner_pullenti.py
@@ -3,9 +3,16 @@ Pullenti recognition is here
 """
 from pullenti.ner.SourceOfAnalysis import SourceOfAnalysis
 
+from doc_info import CompanyInfo
 
-def extract_money(text, processor):
+
+def extract_money_org(text, processor, doc):
     result = processor.process(SourceOfAnalysis(text))
 
     for entity in result.entities:
-        print(entity.value) if entity.type_name == 'MONEY' else ""
+        doc.money.add(entity.value) if entity.type_name == 'MONEY' else None
+        if entity.type_name == 'ORGANIZATION':
+            company = CompanyInfo(str(entity))
+            doc.companies.add(company.company)
+        # print(entity.value) if entity.type_name == 'MONEY' else ""
+        print(entity) if entity.type_name == 'ORGANIZATION' else ""
